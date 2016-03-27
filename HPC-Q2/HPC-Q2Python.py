502beb75da3dd53cde3e74d1ee2f5d1f711f3de0
hyo13 <ho.or13@imperial.ac.uk>
2016-03-27 04:34:07 +0100

    #5 Complete Q2b Python Plot

--- HPC-Q2/HPC-Q2Python.py
+++ HPC-Q2/HPC-Q2Python.py
@@ -9,7 +9,8 @@ import subprocess
 import matplotlib
 import matplotlib.pylab as plt
 import numpy
-import numpy as np 
+import numpy as np
+import math
 
 #Inputs
 L=1
@@ -28,15 +29,34 @@ inputs='./a.out'+' '+repr(L)+' '+repr(Nx)+' '+repr(T)+' '+repr(Nt)+' '+repr(alph
 subprocess.call('g++ main.cpp',shell=True)
 #Run C++ program and obtain output results
 output=subprocess.Popen(inputs, shell=True, stdout=subprocess.PIPE)
-u=output.communicate()[0]
+U=output.communicate()[0]
 #Convert string to array
-u=[float(s) for s in u.split()] 
-U=np.array(u)
+U=[float(s) for s in U.split()] 
+U=np.array(U)
 
 #Create time array
-t = [];
+T=[]
 for i in range(0,Nt):
-    t.append(i*dt)
-T=np.array(t)
+    T.append(i*dt)
+T=np.array(T)
 
+#Calculate Analytical Solution
+Ut=[]
+for i in range(0,Nt):
+    t=T[i]
+    Utt=[]
+    for n in range(1,101):
+        Utt.append((8*math.sin(n*math.pi/2)**2/(math.pi**3*n**3))*math.sin(n*math.pi/2)*math.exp(-alpha*t*n**2*math.pi**2/L**2))
+    Ut.append(np.sum(Utt))
+Ut=np.array(Ut)
 
+#Numerical Solution: Plot U against T
+plt.plot(T,U,'b',label="Numerical Solution")
+#Analysitcal Solution: Plot Ut against T
+plt.plot(T,Ut,'r--',label="Analytical Solution")
+#Plot Configurations
+plt.grid(b=None, which='major', axis='both')
+plt.legend(loc='best')
+plt.title('Numerical Solution vs Analytical Solution of 1D Heat Equation')
+plt.xlabel('Time (s)')
+plt.ylabel('Solution U')
