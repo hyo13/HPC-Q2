7104a09b3619e903b10884139c26edfd3b3bdd86
hyo13 <ho.or13@imperial.ac.uk>
2016-03-27 17:04:37 +0100

    #7 Q2c Complete

--- HPC-Q2/HPC-Q2Python.py
+++ HPC-Q2/HPC-Q2Python.py
@@ -35,28 +35,28 @@ U=[float(s) for s in U.split()]
 U=np.array(U)
 
 #Create time array
-T=[]
+TIME=[]
 for i in range(0,Nt):
-    T.append(i*dt)
-T=np.array(T)
+    TIME.append(i*dt)
+TIME=np.array(TIME)
 
 #Calculate Analytical Solution
 Ut=[]
 for i in range(0,Nt):
-    t=T[i]
+    t=TIME[i]
     Utt=[]
-    for n in range(1,101):
+    for n in range(1,21):
         Utt.append((8*math.sin(n*math.pi/2)**2/(math.pi**3*n**3))*math.sin(n*math.pi/2)*math.exp(-alpha*t*n**2*math.pi**2/L**2))
     Ut.append(np.sum(Utt))
 Ut=np.array(Ut)
 
 #Numerical Solution: Plot U against T
-plt.plot(T,U,'b',label="Numerical Solution")
+plt.plot(TIME,U,'b',label="Numerical Solution")
 #Analysitcal Solution: Plot Ut against T
-plt.plot(T,Ut,'r--',label="Analytical Solution")
+plt.plot(TIME,Ut,'r--',label="Analytical Solution")
 #Plot Configurations
 plt.grid(b=None, which='major', axis='both')
 plt.legend(loc='best')
 plt.title('Numerical Solution vs Analytical Solution of 1D Heat Equation')
-plt.xlabel('Time (s)')
+plt.xlabel('Time (units)')
 plt.ylabel('Solution U')
--- HPC-Q2/HPC-Q2cPython.py
+++ HPC-Q2/HPC-Q2cPython.py
@@ -16,47 +16,71 @@ import math
 L=1
 Nx=20
 T=1
-Nt=800
+Nt=2000
 alpha=1
 
-while (Nt<=2000):
-    #Initial Calculations
-    dt=T/Nt
-    
-    #Convert inputs to string to run C++ program
-    inputs='./a.out'+' '+repr(L)+' '+repr(Nx)+' '+repr(T)+' '+repr(Nt)+' '+repr(alpha)
-    
-    #Compile C++ program
-    subprocess.call('g++ main.cpp',shell=True)
-    #Run C++ program and obtain output results
-    output=subprocess.Popen(inputs, shell=True, stdout=subprocess.PIPE)
-    U=output.communicate()[0]
-    #Convert string to array
-    U=[float(s) for s in U.split()] 
-    U=np.array(U)
-    
-    #Create time array
-    T=[]
-    for i in range(0,Nt):
-        T.append(i*dt)
-    T=np.array(T)
-    
-    #Calculate Analytical Solution
-    Ut=[]
-    for i in range(0,Nt):
-        t=T[i]
-        Utt=[]
-        for n in range(1,101):
-            Utt.append((8*math.sin(n*math.pi/2)**2/(math.pi**3*n**3))*math.sin(n*math.pi/2)*math.exp(-alpha*t*n**2*math.pi**2/L**2))
-        Ut.append(np.sum(Utt))
-    Ut=np.array(Ut)
-    
-    #Calulate error in each term
-    RMSt=[]
-    RMSt=Ut-U
-    #Calculate RMS Error
+#Compile C++ program
+subprocess.call('g++ main.cpp',shell=True)
+
+while (Nx<=30):
+
+    #Initialize dt and RMS array
+    DT=[]
     RMS=[]
-    RMS.append((RMSt**2/Nt)**0.5)
     
-    #Loop Increment
-    Nt=Nt+50
+    #Calculate RMS Error for Nt between 800 and 2000 at increments of 50
+    Nt=2000
+    while (Nt<=3000):
+        #Initial Calculations
+        dt=T/Nt
+        DT.append(dt)
+        
+        #Convert inputs to string to run C++ program
+        inputs='./a.out'+' '+repr(L)+' '+repr(Nx)+' '+repr(T)+' '+repr(Nt)+' '+repr(alpha)
+        
+        #Run C++ program and obtain output results
+        output=subprocess.Popen(inputs, shell=True, stdout=subprocess.PIPE)
+        U=output.communicate()[0]
+        #Convert string to array
+        U=[float(s) for s in U.split()] 
+        U=np.array(U)
+        
+        #Create time array
+        TIME=[]
+        for i in range(0,Nt):
+            TIME.append(i*dt)
+        TIME=np.array(TIME)
+        
+        #Calculate Analytical Solution
+        Ut=[]
+        for i in range(0,Nt):
+            t=TIME[i]
+            Utt=[]
+            for n in range(1,21):
+                Utt.append((8*math.sin(n*math.pi/2)**2/(math.pi**3*n**3))*math.sin(n*math.pi/2)*math.exp(-alpha*t*n**2*math.pi**2/L**2))
+            Ut.append(np.sum(Utt))
+        Ut=np.array(Ut)
+        
+        #Calculate RMS Error
+        RMS.append((np.sum((Ut-U)**2)/Nt)**0.5)    
+        
+        #Nt Increment
+        print("Nt= ",Nt)
+        Nt=Nt+50
+        
+    #Convert List to Array
+    RMS=np.array(RMS)
+    #Plot RMS vs DT
+    name='Nx= '+repr(Nx)
+    plt.plot(DT,RMS,label=name)
+    
+    #Nx Increment
+    print("Nx= ",Nx)
+    Nx=Nx+2
+
+#Plot Configurations
+plt.grid(b=None, which='major', axis='both')
+plt.legend(loc='best')
+plt.title('Root Mean Squared Error of Forward Euler Time Integration')
+plt.xlabel('dt (units)')
+plt.ylabel('dx (units)')
